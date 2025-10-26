85c010fcf842b28eaac49e1faaed5aa3448a36ec
Torryy00 <votakmasha@gmail.com>
2025-10-26 17:20:31 +0100

    final

--- app/notes/[id]/NoteDetails.client.tsx
+++ app/notes/[id]/NoteDetails.client.tsx
@@ -1,8 +1,8 @@
 "use client";
 
 import { useQuery } from "@tanstack/react-query";
-import { fetchNoteById } from ../lib/api";
 import { useParams } from 'next/navigation';
+import { fetchNoteById } from "@/lib/api";
 import css from "./NoteDetails.module.css";
 
 const NoteDetailsClient = () => {
